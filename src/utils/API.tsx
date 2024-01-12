7e9652ea7eadfbbff9b3942fb664e76b528cfa12
dragos <dragosflow@yahoo.com>
2024-01-12 12:32:54 +0200

    chore: delete logs

--- src/utils/API.tsx
+++ src/utils/API.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
     
 async function getWeatherData(city: string) {
   const apiKey = process.env.REACT_APP_API_KEY;
-  console.log('API Key:', apiKey); // Verifică dacă cheia API este prezentă
 
   try {
     const url = `https://api.api-ninjas.com/v1/weather?city=${city}`;
@@ -16,7 +15,7 @@ async function getWeatherData(city: string) {
     return response.data;
   } catch (error: any) {
     console.error('Request failed:', error.message);
-    return null; // Returnează null pentru a indica o eroare
+    return null;
   }
 }
 
